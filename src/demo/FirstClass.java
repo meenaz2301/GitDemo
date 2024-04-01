7db8887601c3277780fbe3b270357953e2f40213
meenaz2301 <meenazrizve@gmail.com>
2024-04-01 23:03:09 +0530

    one update

--- src/demo/FirstClass.java
+++ src/demo/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 		// TODO Auto-generated method stub
 		System.out.println("hello first class");
 		System.out.println("git demo have change something");
+		System.out.println("only on udate here");
 	}
 
 }
