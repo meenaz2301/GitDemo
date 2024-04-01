8b2d8a04441e744aaeb2b6743b331d9eca979a57
meenaz2301 <meenazrizve@gmail.com>
2024-04-01 22:58:14 +0530

    demo guy

--- src/demo/SecondClass.java
+++ src/demo/SecondClass.java
@@ -5,6 +5,7 @@ public class SecondClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello Second class");
+		System.out.println("git demo make 2nd change");
 	}
 
 }
